9e21a7dcea1b24ed1e69b6fd85f3d6efcff3c72b
MUHAMMED AYIMEN <aiymenlatheef03@gmail.com>
2021-09-11 14:39:55 +0530

    Added: 3 Containers for BMI Calculation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Form from './components/Form';
+import BmiScore from './components/BmiScore'
+import BmiList from './components/BmiList'
 
 function App() {
+  let bmi = 18;
+  const changeBmi = () =>{
+    bmi = 19;
+    console.log(bmi);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div>BMI CALCULATOR</div>
+    <div>my Bmi is {bmi}</div>
+    <button onClick={changeBmi}>Click</button>
+    <Form/>
+    <BmiScore/>
+    <BmiList/>
+    </>
   );
 }
 
