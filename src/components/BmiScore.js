7d3805f7565fd15c58d564cacd3ea9a1b34f4fb7
MUHAMMED AYIMEN <aiymenlatheef03@gmail.com>
2021-10-16 01:08:15 +0530

    Added: Condition for BMI in bmiScore box

--- src/components/BmiScore.js
+++ src/components/BmiScore.js
@@ -1,4 +1,4 @@
-function BmiScore({ bmiNo, bmiName }) {
+function BmiScore({ bmiNo, bmiName, changeWeight }) {
   return (
     <div className="text-center shadow rounded p-4">
       <div>Your BMI Score</div>
@@ -8,6 +8,21 @@ function BmiScore({ bmiNo, bmiName }) {
         </div>
       </div>
       <div className="fs-3 fw-bold text-primary">{bmiName}</div>
+      {changeWeight.type === "positive" && (
+        <div className="fs-4">
+          "You need to lose
+          <span className="fw-bold">{changeWeight.wight} kg"</span>
+        </div>
+      )}
+      {changeWeight.type === "negative" && (
+        <div className="fs-4">
+          "You need to gain
+          <span className="fw-bold">{changeWeight.wight} kg"</span>
+        </div>
+      )}
+      {changeWeight.type === "normal" && (
+        <div className="fs-4">"You weight is Normal,Good for you" </div>
+      )}
     </div>
   );
 }
