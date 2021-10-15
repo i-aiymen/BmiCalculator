0953895e3930c06f6b0438010c4f3e4bf617a9ca
MUHAMMED AYIMEN <aiymenlatheef03@gmail.com>
2021-10-16 00:15:26 +0530

    Added: box for displaying bmi

--- src/components/BmiScore.js
+++ src/components/BmiScore.js
@@ -1,11 +1,15 @@
-function BmiScore({bmiNo,bmiName}) { 
-    return (
-        <div>
-            Bmi Score {bmiNo} <br/>
-            Bmi Type {bmiName}
+function BmiScore({ bmiNo, bmiName }) {
+  return (
+    <div className="text-center shadow rounded p-4">
+      <div>Your BMI Score</div>
+      <div className="row justify-content-md-center">
+        <div className="p-3 my-2 alert fs-1 alert-primary col-sm-4">
+          {bmiNo}
         </div>
-        
-    )
+      </div>
+      <div className="fs-3 fw-bold text-primary">{bmiName}</div>
+    </div>
+  );
 }
 
-export default BmiScore
+export default BmiScore;
